1b215e590916f12df872af8c6ba1452dd96cadd9
severs65 <2360787790@qq.com>
2026-08-27 13:55:42 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,43 +1,48 @@
-from flask import Flask, request
-from flask_socketio import SocketIO, emit
+from flask import Flask, request, jsonify
+import time
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'p2p_signal_2026'
-socketio = SocketIO(app, cors_allowed_origins="*", async_mode="gevent")
 
-# 网络池字典：key=池名称，value=池内所有玩家sid和公网地址
-network_pools = {}
+# 网络池：key=池名, value={玩家地址: 最后心跳时间}
+pools = {}
 
-@socketio.on('join_pool')
-def handle_join_pool(data):
-    pool_name = data['pool_name']
-    public_addr = data['public_addr']  # 客户端上传自己的公网IP:端口
-    sid = request.sid
+@app.route('/join', methods=['POST'])
+def join():
+    data = request.get_json()
+    pool_name = data['pool']
+    addr = data['addr']  # 格式：ip:port
+    if pool_name not in pools:
+        pools[pool_name] = {}
+    pools[pool_name][addr] = time.time()
+    return jsonify({"members": list(pools[pool_name].keys())})
 
-    if pool_name not in network_pools:
-        network_pools[pool_name] = {}
-    # 存入当前玩家信息
-    network_pools[pool_name][sid] = public_addr
-    # 把池内全部玩家列表广播给池里所有人
-    emit('pool_member_list', {
-        "pool": pool_name,
-        "members": network_pools[pool_name]
-    }, room=pool_name)
+@app.route('/list', methods=['GET'])
+def list_members():
+    pool_name = request.args.get('pool')
+    if pool_name not in pools:
+        return jsonify({"members": []})
+    # 自动清理超过60秒没心跳的掉线玩家
+    now = time.time()
+    pools[pool_name] = {a: t for a, t in pools[pool_name].items() if now - t < 60}
+    return jsonify({"members": list(pools[pool_name].keys())})
 
-@socketio.on('leave_pool')
-def handle_leave_pool(data):
-    pool_name = data['pool_name']
-    sid = request.sid
-    if pool_name in network_pools and sid in network_pools[pool_name]:
-        del network_pools[pool_name][sid]
-        # 没人就删掉这个池
-        if len(network_pools[pool_name]) == 0:
-            del network_pools[pool_name]
-        else:
-            emit('pool_member_list', {
-                "pool": pool_name,
-                "members": network_pools[pool_name]
-            }, room=pool_name)
+@app.route('/heartbeat', methods=['POST'])
+def heartbeat():
+    data = request.get_json()
+    pool_name = data['pool']
+    addr = data['addr']
+    if pool_name in pools and addr in pools[pool_name]:
+        pools[pool_name][addr] = time.time()
+    return jsonify({"ok": True})
+
+@app.route('/leave', methods=['POST'])
+def leave():
+    data = request.get_json()
+    pool_name = data['pool']
+    addr = data['addr']
+    if pool_name in pools and addr in pools[pool_name]:
+        del pools[pool_name][addr]
+    return jsonify({"ok": True})
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0')
+    app.run(host='0.0.0.0', port=5000)
